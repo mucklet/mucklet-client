400cc599f4dc95b00feb1632cf57a668a5942c8f
Accipiter Nisus <accipiter@gmail.com>
2022-12-29 15:46:14 +0100

    GH-34: Created separate copyCharId module. Added copying of char ID to pageReports module. Added Character ID to inspect command. Made copyToClipboard use Clipboard API primarily.

--- src/client/modules/moderator/moderatorAddons/menuItem/menuItemCopyId/MenuItemCopyId.js
+++ src/client/modules/moderator/moderatorAddons/menuItem/menuItemCopyId/MenuItemCopyId.js
@@ -1,6 +1,4 @@
-import { Elem, Txt } from 'modapp-base-component';
 import l10n from 'modapp-l10n';
-import copyToClipboard from 'utils/copyToClipboard';
 
 /**
  * MenuItemCopyId adds the char log menu item to copy character ID.
@@ -11,7 +9,7 @@ class MenuItemCopyId {
 
 		this.app.require([
 			'charLog',
-			'toaster',
+			'copyCharId',
 		], this._init.bind(this));
 	}
 
@@ -29,19 +27,9 @@ class MenuItemCopyId {
 	}
 
 	_copy(charId, ev) {
-		let c = ev.char;
-		if (!c) return;
-		copyToClipboard("#" + c.id);
-		this.module.toaster.open({
-			title: l10n.l('menuItemCopyId.copiedToClipboard', "Copied to clipboard"),
-			content: close => new Elem(n => n.elem('div', [
-				n.component(new Txt(l10n.l('menuItemCopyId.copiedIdOfCharacter', "Copied ID of character:"), { tagName: 'p' })),
-				n.component(new Txt(c.name + " " + c.surname, { tagName: 'p', className: 'dialog--strong' })),
-			])),
-			closeOn: 'click',
-			type: 'success',
-			autoclose: true,
-		});
+		if (ev.char) {
+			this.module.copyCharId.copy(ev.char);
+		}
 	}
 
 	dispose() {
