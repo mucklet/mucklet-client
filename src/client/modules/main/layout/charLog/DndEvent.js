384964cd7cf02dbb2899e9470a9c6494b40d6919
Accipiter Nisus <accipiter@gmail.com>
2023-03-07 16:09:14 +0100

    GH-74: Updated Do not Disturb fieldset label.

--- src/client/modules/main/layout/charLog/DndEvent.js
+++ src/client/modules/main/layout/charLog/DndEvent.js
@@ -8,7 +8,7 @@ class DndEvent extends Elem {
 		super(n => n.elem('div', [
 			n.elem('div', { className: 'charlog--fieldset' }, [
 				n.elem('div', { className: 'charlog--fieldset-label' }, [
-					n.component(new Txt(l10n.l('charLog.notSentTo', "Not sent to {name}", { name: c && c.name }))),
+					n.component(new Txt(l10n.l('charLog.notSentTo', "{fullname} is set as Do not Disturb", { fullname: c && (c.name + " " + c.surname).trim() }))),
 				]),
 				n.component(new Html(formatText(ev.msg), { tagName: 'span', className: 'common--formattext' })),
 			]),
