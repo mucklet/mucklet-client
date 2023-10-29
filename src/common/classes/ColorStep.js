fbc8bb8a83ff96953ffc64acb9730fd367b3d198
Accipiter Nisus <accipiter@gmail.com>
2023-10-29 13:53:44 +0100

    GH-149: Rewrite of ColorStep to use DelimStep and RegexStep to more clearly show that the leading # symbol is correct.

--- src/common/classes/ColorStep.js
+++ src/common/classes/ColorStep.js
@@ -1,9 +1,11 @@
+import DelimStep from 'classes/DelimStep';
+import RegexStep from 'classes/RegexStep';
 import Err from './Err';
 
 /**
  * ColorStep consumes a hex color code Eg. #c1a657
  */
-class ColorStep {
+class ColorStep extends DelimStep {
 
 	/**
 	 * Creates an instance of ColorStep.
@@ -11,65 +13,36 @@ class ColorStep {
 	 * @param {object} [opt] Optional params.
 	 * @param {string} [opt.name] Name used in error outputs. Defaults to the id value.
 	 * @param {string} [opt.token] Token name. Defaults to 'entityid'.
+	 * @param {string} [opt.delimToken] Delimiter token name. Defaults to 'delim'.
 	 * @param {Step} [opt.next] Next step after a matched color code.
 	 * @param {Step} [opt.else] Step after if the color code is missing. Will disabled any errRequired set.
 	 * @param {?function} [opt.errRequired] Callback function that returns an error when it fails to match. Null means it is not required.: function(this)
 	 * @param {function} [opt.errInvalid] Callback function that returns an invalid error if the # is not followed by a valid color code.
 	 */
 	constructor(id, opt) {
-		opt = opt || {};
-		this.id = id;
-		this.name = opt.name || id;
-		this.token = opt.token || 'entityid';
-		this.next = opt.next || null;
-		this.else = opt.else || null;
-		this.errRequired = opt.hasOwnProperty('errRequired')
-			? opt.errRequired
-			: self => new Err('idStep.required', 'There is no {name}. Expected a #-sign followed by 3 or 6 hexadecimal digits.', { name: self.name });
-		this.errInvalid = opt.errInvalid || ((self, m) => (
+		let errInvalid = opt?.errInvalid || ((self, m) => (
 			new Err('idStep.invalid', 'The {name} value "#{value}" is invalid. Expected the #-sign to be followed by 3 or 6 hexadecimal digits.', { name: self.name, value: m })
 		));
-	}
-
-	parse(stream, state) {
-		if (!stream) {
-			return this._setRequired(state);
-		}
-
-		stream.eatSpace();
-
-		let c = stream.next();
-		if (c !== '#') {
-			state.backUp(stream);
-			return this._setRequired(state);
-		}
-
-		// Match until next word break
-		let m = stream.match(/^\w*\b/);
-		// Validate the match
-		if (!m || !m[0].match(/^(?:[0-9a-fA-F]{3,3}){1,2}$/)) {
-			state.setError(this.errInvalid(this, m));
-			return 'error';
-		}
-
-		// Add colorcode with lowercase
-		state.setParam(this.id, '#' + m[0].toLowerCase());
-
-		// Add followup steps
-		if (this.next) {
-			state.addStep(this.next);
-		}
-
-		return this.token;
-	}
-
-	_setRequired(state) {
-		if (this.else) {
-			state.addStep(this.else);
-		} else if (this.errRequired) {
-			state.setError(this.errRequired(this));
-		}
-		return false;
+		let regexStep = new RegexStep(id, /^\w*\b/, {
+			name: opt?.name || id,
+			token: opt?.token || 'listitem',
+			errRequired: self => errInvalid(self, ''),
+			validate: (self, value) => !value.match(/^(?:[0-9a-fA-F]{3,3}){1,2}$/)
+				? errInvalid(self, value)
+				: null,
+			prepareValue: str => '#' + str.toLowerCase(),
+			next: opt?.next,
+		});
+		super('#', {
+			token: opt?.delimToken || 'listitem',
+			errRequired: opt.hasOwnProperty('errRequired')
+				? opt.errRequired
+				: self => new Err('idStep.required', 'There is no {name}. Expected a #-sign followed by 3 or 6 hexadecimal digits.', { name: self.name }),
+			next: [
+				regexStep,
+			],
+			else: opt?.else,
+		});
 	}
 }
 
