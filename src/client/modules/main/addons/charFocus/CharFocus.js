9ad77d592e91ebe090da73da0c6590cc72b4a852
Kelmi <kelmi.koira@pm.me>
2024-03-13 05:39:50 +0200

    Add documentation for the public interface

--- src/client/modules/main/addons/charFocus/CharFocus.js
+++ src/client/modules/main/addons/charFocus/CharFocus.js
@@ -127,7 +127,7 @@ class CharFocus {
 		if (!isValidColor(color)) {
 			color = this.focusColors[char.id] || this._getColor(ctrlId);
 		}
-		
+
 		if (!noUpdate) {
 			this.focusColors[char.id] = color;
 			this._saveFocusColors();
@@ -142,10 +142,19 @@ class CharFocus {
 		return this;
 	}
 
+	/**
+	 * Returns a CharList with the currently focused characters
+	 * @returns	{CharList}	Focused characters of the currently controlled character.
+	 */
 	getFocusCharList() {
 		return this.focusCharList;
 	}
 
+	/**
+	 * Returns an object with focused character ids as keys and their focus colors as values.
+	 * Shared between all controlled characters.
+	 * @returns { [charId: string]: string }	Object containing focus character -> color mapping.
+	 */
 	getFocusCharColors() {
 		return this.focusColors;
 	}
