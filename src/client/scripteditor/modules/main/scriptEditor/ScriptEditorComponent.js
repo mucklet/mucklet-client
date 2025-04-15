06afcce5fc2de9fb939469ff5e8085699c03b1b6
Accipiter Nisus <accipiter@gmail.com>
2025-04-15 19:45:10 +0200

    GH-363: Work on ScriptEditorComponent. Incomplete.

--- src/client/scripteditor/modules/main/scriptEditor/ScriptEditorComponent.js
+++ src/client/scripteditor/modules/main/scriptEditor/ScriptEditorComponent.js
@@ -1,6 +1,7 @@
 import { Elem, Txt } from 'modapp-base-component';
-// import { ModelComponent } from 'modapp-resource-component';
-// import Fader from 'components/Fader';
+import { ModelTxt } from 'modapp-resource-component';
+import l10n from 'modapp-l10n';
+// import { CollectionWrapper } from 'modapp-resource';
 
 /**
  * ScriptEditorComponent renders the script editor.
@@ -15,7 +16,60 @@ class ScriptEditorComponent {
 
 	render(el) {
 		this.elem = new Elem(n => n.elem('div', { className: 'scripteditor' }, [
-			n.component(new Txt("Script editor")),
+			n.elem('div', { className: 'scripteditor--header' }, [
+				// n.component(new Context(
+				// 	() => new CollectionWrapper(this.module.self.getTools(), {
+				// 		filter: t => !t.type || t.type == 'header' && (t.filter ? t.filter() : true),
+				// 	}),
+				// 	tools => tools.dispose(),
+				// 	tools => new CollectionList(
+				// 		tools,
+				// 		t => t.componentFactory(),
+				// 		{
+				// 			className: 'scripteditor--headertools',
+				// 			subClassName: t => t.className || null,
+				// 			horizontal: true,
+				// 		},
+				// 	),
+				// )),
+				n.elem('div', { className: 'scripteditor--headercontent' }, [
+					// Title
+					n.component(new Txt("Script editor", { tagName: 'h2', className: 'scripteditor--headertitle' })),
+
+					// Buttons
+					n.elem('div', { className: 'scripteditor--headerbuttons' }, [
+						n.elem('button', {
+							events: { click: () => this._onSave() },
+							className: 'btn primary scripteditor--save',
+						}, [
+							n.component(new ModelTxt(this.model, m => m.isModified
+								? l10n.l('scriptEditor.saveChanges', "Save changes")
+								: l10n.l('scriptEditor.close', "Close"),
+							)),
+						]),
+					]),
+				]),
+
+
+				// n.component(new Context(
+				// 	() => new CollectionWrapper(this.module.self.getTools(), {
+				// 		filter: t => !t.type || t.type == 'logo' && (t.filter ? t.filter() : true),
+				// 	}),
+				// 	tools => tools.dispose(),
+				// 	tools => new CollectionList(
+				// 		tools,
+				// 		t => t.componentFactory(),
+				// 		{
+				// 			className: 'scripteditor--logotools flex-1 flex-row sm',
+				// 			subClassName: t => t.className || null,
+				// 			horizontal: true,
+				// 		},
+				// 	),
+				// )),
+			]),
+			n.elem('div', { className: 'scripteditor--main' }, [
+				n.component(new Txt("Script code")),
+			]),
 		]));
 		return this.elem.render(el);
 	}
@@ -25,6 +79,10 @@ class ScriptEditorComponent {
 			this.elem.unrender();
 			this.elem = null;
 		}
+	}
+
+	_onSave() {
+		console.error("not implemented");
 	}
 }
 
