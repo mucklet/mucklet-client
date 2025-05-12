a928546c689b8c19349521e1df803ebc88378547
Accipiter Nisus <accipiter@gmail.com>
2025-05-12 14:29:02 +0200

    GH-372: Added PrivateDescribeEvent. GH-371: Removed describe brackets and used default text font size for describe. Removed aria-hidden from containers such as Slider, Panel, and MobilePanel

--- src/client/modules/main/layout/charLog/DescribeEvent.js
+++ src/client/modules/main/layout/charLog/DescribeEvent.js
@@ -4,9 +4,7 @@ import formatText from 'utils/formatText';
 class DescribeEvent extends Elem {
 	constructor(charId, ev) {
 		super(n => n.elem('div', { className: 'charlog--highlight' }, [
-			n.text('⌈'),
 			n.component(new Html(formatText(ev.msg, ev.mod), { tagName: 'span', className: 'common--formattext' })),
-			n.text('⌋'),
 		]));
 	}
 
