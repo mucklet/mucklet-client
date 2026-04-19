e90707877d9b737a4b07316c5d595a3b906eb1c3
Accipiter Nisus <accipiter@gmail.com>
2026-04-19 21:15:58 +0200

    GH-475: Removed obsolete configs and build commands. Added default realm image and icon.

--- cfg/client/site.config.local.js
+++ cfg/client/site.config.local.js
@@ -1,22 +1,23 @@
 // Configuration for the site
 
 module.exports = {
-	APP_ISTEMPLATE: true,
+	APP_ISTEMPLATE: false,
 	APP_VERSION: '0', // Overwritten by version in package.json
-	APP_TITLE: '', // Set in template
-	APP_DESCRIPTION: '', // Set in template
-	APP_ROOT: '', // Set in template
-	APP_IMAGE: null, // Set in template
-	APP_ICON: null, // Set in template
+	APP_TITLE: "Dev Realm",
+	APP_DESCRIPTION: "A textual world of role play. Create a character, wake them up, and join in.",
+	APP_ROOT: '/',
+	APP_IMAGE: { id: 'd7d4kt4ks8oet37c6fo0', rendering: '' },
+	APP_ICON: { id: 'd7ib1dkks8o3q6599r0g', rendering: '' },
 
-	API_HOST_PATH: '', // Set in template
-	API_WEBRESOURCE_PATH: '', // Set in template
-	API_FILE_PATH: '', // Set in template
-	API_CROSS_ORIGIN: true,
+	API_HOST_PATH: 'ws://localhost:8080/',
+	API_WEBRESOURCE_PATH: 'http://localhost:8080/api/',
+	API_FILE_PATH: 'http://localhost:6452/',
+	API_IDENTITY_PATH: '/identity/',
+	API_CROSS_ORIGIN: false,
 
-	AUTH_LOGIN_URL: 'http://localhost:6453/oauth2/login',
-	AUTH_LOGOUT_URL: 'http://localhost:6453/oauth2/logout',
-	AUTH_AUTHENTICATE_URL: 'http://localhost:6453/authenticate',
+	AUTH_LOGIN_URL: '/auth/oauth2/login',
+	AUTH_LOGOUT_URL: '/auth/oauth2/logout',
+	AUTH_AUTHENTICATE_URL: '/auth/authenticate',
 	AUTH_LOGIN_RID: 'auth',
 	AUTH_AUTHENTICATE_RID: 'auth',
 
---
+++ cfg/client/site.config.realmclient.js
@@ -0,0 +1,24 @@
+// Configuration for the site
+
+module.exports = {
+	APP_ISTEMPLATE: true,
+	APP_VERSION: '0', // Overwritten by version in package.json
+	APP_TITLE: '', // Set in template
+	APP_DESCRIPTION: '', // Set in template
+	APP_ROOT: '', // Set in template
+	APP_IMAGE: null, // Set in template
+	APP_ICON: null, // Set in template
+
+	API_HOST_PATH: '', // Set in template
+	API_WEBRESOURCE_PATH: '', // Set in template
+	API_FILE_PATH: '', // Set in template
+	API_CROSS_ORIGIN: true,
+
+	AUTH_LOGIN_URL: 'http://localhost:6453/oauth2/login',
+	AUTH_LOGOUT_URL: 'http://localhost:6453/oauth2/logout',
+	AUTH_AUTHENTICATE_URL: 'http://localhost:6453/authenticate',
+	AUTH_LOGIN_RID: 'auth',
+	AUTH_AUTHENTICATE_RID: 'auth',
+
+	HUB_PATH: 'http://localhost:6460/',
+};
